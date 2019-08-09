76020810d5594f56db82ad5da889c2cf330f3bfd
theOluwafemi <oluwafemidamilola21@gmail.com>
2019-08-09 07:28:20 +0100

    remove employee

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,99 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Employees</h1>
+
+    <EmployeeForm @add:employee="addEmployee" />
+    <br>
+    <EmployeeTable 
+      :employees = 'employees' 
+      @delete:employee="deleteEmployee"
+      @edit:employee="editEmployee"
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import EmployeeTable from './components/EmployeeTable'
+import EmployeeForm from './components/EmployeeForm'
+import { constants } from 'crypto';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
-  }
+    EmployeeTable,
+    EmployeeForm
+  },
+
+  data() {
+    return {
+      employees: []
+    }
+  },
+
+  methods: {
+    async getEmployees() {
+      try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users')
+        const data = await response.json()
+        this.employees = data
+      } catch (error) {
+        console.error(error)
+      }
+    },
+
+    async addEmployee(employee) {
+      try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users', {
+          method: 'POST',
+          body: JSON.stringify(employee),
+          headers: { 'Content-type': 'appliction/json; charset=UTF-8' }
+        })
+        const data = await response.json()
+        this.employees = [...this.employees, data]
+      } catch (error) {
+        console.error(errror)
+      }
+    },
+
+    async editEmployee(id, updatedEmployee) {
+      try {
+        const response = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`, {
+          method: 'PUT',
+          body: JSON.stringify(updatedEmployee),
+          headers: { 'Content-type': 'application/json; charset=UTF-8' },        
+        })
+        const data = await response.json()
+        this.employees = this.employees.map(employee => (employee.id === id ? data : employee))
+      } catch (error) {
+        console.error(error)
+      }
+    },
+
+    async deleteEmployee(id) {
+      try {
+        await fetch(`https://jsonplaceholder.typicode.com/users/${id}`, {
+          method: "DELETE"
+        });
+        this.employees = this.employees.filter(employee => employee.id !== id);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  },
+
+  mounted() {
+    this.getEmployees()
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+    margin-top: 30px;
+    padding: 60px;
+  }
 </style>
---
+++ src/components/EmployeeTable.vue
@@ -0,0 +1,87 @@
+<template>
+  <div>
+    <div id="employee-table">
+      <p v-if="employees.length < 1" class="empty-table">
+        No employees
+      </p>
+      <table v-if="employees.length > 0" >
+        <thead>
+          <tr>
+            <th>Employee name</th>
+            <th>Employee email</th>
+            <th>Options</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="employee in employees" :key="employee.id">
+            <td v-if="editing === employee.id">
+              <input type="text" v-model="employee.name">
+            </td>
+            <td v-else>{{employee.name}}</td>
+
+            <td v-if="editing === employee.id">
+              <input type="text" v-model="employee.email" />
+            </td>
+            <td v-else>{{employee.email}}</td>
+
+
+            <td v-if="editing === employee.id">
+              <button @click="editEmployee(employee)">Save</button>
+              <button class="muted-button" @click="editing = null">Cancel</button>
+            </td>
+            <td v-else>
+              <button @click="editMode(employee.id)">Edit</button>
+              <button @click="$emit('delete:employee', employee.id)">Delete</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'employee-table',
+  props: {
+    employees: Array,
+  },
+  data() {
+    return {
+      editing: null
+    }
+  },
+  methods: {
+    editMode(id) {
+      this.editing = id
+    },
+
+    editEmployee(employee) {
+      if (employee.name === '' || employee.email === '') return
+      this.$emit('edit:employee', employee.id, employee)
+      this.editing = null
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+button {
+  margin: 0 0.5rem 0 0;
+}
+</style>
